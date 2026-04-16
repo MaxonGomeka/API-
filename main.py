24a62a85c6b551667c4c11cc5f7710271b095589
MaxonGomeka <zuzutech200@gmail.com>
2026-04-17 00:04:10 +0200

    OK

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 app = FastAPI()
 
+#requests Get Method URL
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Home of API"}
+
+@app.get("/posts")
+def get_posts():
+    return {"message": "Get somes posts"}
+
+@app.post("/creatposts")
+def create_posts(payload: dict = Body(...)):
+    print (payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
